c3130e743f56d29b9297fd7603b4592830eaae65
Christopher Stephenson <hpufo2@gmail.com>
2017-09-20 11:41:14 -0400

    changed project settings

--- src/app.js
+++ src/app.js
@@ -1,7 +1,4 @@
+import React from "react";
+import ReactDOM from "react-dom";
 
-document.addEventListener("DOMContentLoaded", function(event) {
-	ReactDOM.render(
-	  <h1>Hello, world!</h1>,
-	  document.getElementById('root')
-	);
-});
+ReactDOM.render(<h1>Hello, world!</h1>, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+var debug = process.env.NODE_ENV !== "production";
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  context: path.join(__dirname, "src"),
+  devtool: debug ? "inline-sourcemap" : null,
+  entry: "./app.js",
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/, 
+        loader: 'babel-loader',
+        query: {
+          presets: [['es2015', { modules: false }], 'react', 'stage-0'],
+          plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy'],
+        }
+      }
+    ]
+  },
+  output: {
+    path: __dirname + "/build/",
+    filename: "app.min.js"
+  },
+  plugins: debug ? [] : [
+    new webpack.LoaderOptionsPlugin({minimize: true, debug: false}),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.OccurrenceOrderPlugin(true),
+    new webpack.optimize.ModuleConcatenationPlugin(),
+    new webpack.optimize.UglifyJsPlugin(),
+  ],
+};
